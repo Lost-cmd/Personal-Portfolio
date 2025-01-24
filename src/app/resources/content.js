54f62abd4082d2ab117fc4ab8c02c8ed509bc282
Physix-x <physix.social@gmail.com>
2025-01-25 02:04:22 +0800

    Updated Post

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -283,7 +283,7 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "Writing about accounting and tech..",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
